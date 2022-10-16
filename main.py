dfdcc750cc9986510082e86ed61ad2c81f88f56a
matheus-roma <matheus.roma@ufpe.br>
2022-10-16 18:52:31 -0300

    starting to mold the city

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils import city
+        
+mycity = city()
+mycity.setup()
+print(mycity.CARS_NUM)
+print(mycity.cars_positions)
---
+++ utils.py
@@ -0,0 +1,22 @@
+class city:
+    def __init__(self) -> None:
+        self.STREETS_WIDTH = 5
+        self.CONSTRUCTIONS_SIZE = 10
+        self.HOR_STREETS = 4
+        self.VER_STREETS = 4
+        self.CARS_NUM = 1
+        self.USERS_NUM = 1
+        self.cars_positions = []
+        self.users_positions = []
+        
+    def setup(self):
+        #self.HOR_STREETS = input("How many horizontal streets?")
+        #self.VER_STREETS = input("How many vertical streets?")
+        self.CARS_NUM = input("How many cars at the beginning?")
+        for i in range(int(self.CARS_NUM)):
+            self.cars_positions.append(list(map(int, input(f"What are the coordinates of the #{i+1} car?").split())))
+        self.USERS_NUM = input("How many users at the beginning?")
+        for i in range(int(self.USERS_NUM)):
+            self.users_positions.append(list(map(int, input(f"What are the coordinates of the #{i+1} user?").split())))
+        
+                    
