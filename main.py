04cbb0023ddc48868aa2b560d24dbc6b77c34747
matheus-roma <matheus.roma@ufpe.br>
2022-10-16 19:21:42 -0300

    starting to model the city

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from utils import city
         
 mycity = city()
-mycity.setup()
-print(mycity.CARS_NUM)
-print(mycity.cars_positions)
+#mycity.setup()
+print("="*50)
+print(f"There are {mycity.CARS_NUM} cars")
+print(f"Their positions are {mycity.cars_positions}")
+print(f"There are {mycity.USERS_NUM} users")
+print(f"Their positions are {mycity.users_positions}")
+print(f"The city dimensions are {mycity.city_dimensions[0]} x {mycity.city_dimensions[1]}")
+
+print("="*50)
--- utils.py
+++ utils.py
@@ -1,13 +1,17 @@
 class city:
     def __init__(self) -> None:
         self.STREETS_WIDTH = 5
-        self.CONSTRUCTIONS_SIZE = 10
+        self.CONSTRUCTIONS_SIZE = 20
         self.HOR_STREETS = 4
         self.VER_STREETS = 4
         self.CARS_NUM = 1
         self.USERS_NUM = 1
         self.cars_positions = []
         self.users_positions = []
+        self.city_dimensions = [
+            2*self.HOR_STREETS*self.STREETS_WIDTH + 2* (self.HOR_STREETS-1)*self.CONSTRUCTIONS_SIZE,
+            2*self.VER_STREETS*self.STREETS_WIDTH + 2* (self.VER_STREETS-1)*self.CONSTRUCTIONS_SIZE
+        ]
         
     def setup(self):
         #self.HOR_STREETS = input("How many horizontal streets?")
@@ -18,5 +22,9 @@ class city:
         self.USERS_NUM = input("How many users at the beginning?")
         for i in range(int(self.USERS_NUM)):
             self.users_positions.append(list(map(int, input(f"What are the coordinates of the #{i+1} user?").split())))
+        self.city_dimensions = [
+            2*self.HOR_STREETS*self.STREETS_WIDTH + 2* (self.HOR_STREETS-1)*self.CONSTRUCTIONS_SIZE,
+            2*self.VER_STREETS*self.STREETS_WIDTH + 2* (self.VER_STREETS-1)*self.CONSTRUCTIONS_SIZE
+        ]
         
                     
